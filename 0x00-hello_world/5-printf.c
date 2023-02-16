5572a01ef291ba61472a9f5b07642fc9141b4ab7
Mbokola <tonypeaks@gmail.com>
2023-02-16 14:07:43 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main: Entry point
+ *
+ *Description: Use printf on string.
+ *Return: 0 if successful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
