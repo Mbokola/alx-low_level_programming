3444cfbc7b3bb098f3255dde85771ac757e4aac4
Mbokola <tonypeaks@gmail.com>
2023-02-16 14:12:20 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  *main: Entry point
- *
+ *void: returns 0
  *Description: Use printf on string.
  *Return: 0 if successful.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
