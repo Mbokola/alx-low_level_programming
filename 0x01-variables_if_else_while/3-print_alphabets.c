ca21e2cae4089a4de649f20e6d6fc77e660da612
Mbokola <tonypeaks@gmail.com>
2023-02-17 06:59:28 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Program entry point
+ *void - type void
+ *Return: zero if succesful
+ */
+int main(void)
+{
+	char c = 'a';
+	char e = 'A';
+
+	do {
+		putchar(c);
+		c++;
+	} while (c <= 'z');
+	do {
+		putchar(e);
+		e++;
+	} while (e <= 'Z');
+	putchar('\n');
+	return (0);
+}
