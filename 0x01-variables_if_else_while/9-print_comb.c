da99238fc4f5698902e0b9c0c2d3e042452dc8a9
Mbokola <tonypeaks@gmail.com>
2023-02-18 01:17:22 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - program entry point
+ *void: no parameter
+ *print single digits using putchar
+ *Return: zero i succesful
+ */
+
+int main(void)
+{
+	/* Declare variable i */
+
+	int i;
+
+	/* Use for loop to extract digits and convert to char */
+
+	for (i = 0 + '0'; i < 10 + '0'; i++)
+	{
+		putchar(i);
+		if (i < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
