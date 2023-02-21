460ed3bbf0e0e2bc27af9cc1441f7f0d8801fedd
Mbokola <tonypeaks@gmail.com>
2023-02-21 18:50:28 +0000

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *print_alphabet_x10: a -z 10 times
+ *print_alphabet_x10 - a -z 10 times
  */
 
 void print_alphabet_x10(void)
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int i = 97;
 	int times = 0;
 
-	while (times <= 10)
+	while (times < 10)
 	{
 		i = 97;
 		while (i <= 122)
