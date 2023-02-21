b70c865c088eb17fa37c85401e67792379b162f8
Mbokola <tonypeaks@gmail.com>
2023-02-21 14:48:16 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  *Return: zero if succesful
  */
 
-int main()
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,6 +15,6 @@ int main()
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-
+	_putchar('\n);
 	return (0);
 }
