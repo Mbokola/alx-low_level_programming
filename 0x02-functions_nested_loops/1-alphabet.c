4848b2e70a642b443b25c450a22224b8014df19b
Mbokola <tonypeaks@gmail.com>
2023-02-21 15:00:59 +0000

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *main - program entry point
+ *
+ *Return: zero if succesful
+ */
+int main(void)
+{
+	int i = 97;
+
+	for (i; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
