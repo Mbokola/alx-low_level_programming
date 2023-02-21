14d317208750338f6258363ee2b6863c95930219
Mbokola <tonypeaks@gmail.com>
2023-02-21 22:44:35 +0000

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,18 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
-			if (i < 10 && j != 0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			} else if (i >= 10)
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
