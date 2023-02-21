d778ac849e4175fd8f0f4ee55b0440562ff2841e
Mbokola <tonypeaks@gmail.com>
2023-02-21 15:19:45 +0000

    0. _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,11 @@
 #include "main.h"
 /**
- *print_alphabet_x10 - program entry point
+ *main - program entry point
  *
  *Return: zero if succesful
  */
-void print_alphabet_x10(void)
+int main(void)
 {
-	i = 'a';
-	for (i; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	putchar('\n');
+	print_alphabet_x10();
 	return (0);
 }
