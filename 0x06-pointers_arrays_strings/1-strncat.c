3cdc679ad50655af145fb6f622d9570dc6cbe474
Mbokola <tonypeaks@gmail.com>
2023-03-01 20:05:32 +0000

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[i++] = src[j];
 	return (dest);
 }
