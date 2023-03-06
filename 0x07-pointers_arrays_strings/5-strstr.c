d2d466aee1ff638fedd9ebe0524cbbcf7c669d3c
Mbokola <tonypeaks@gmail.com>
2023-03-06 19:17:21 +0000

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strstr - finds substring
+ *@haystack: pointer to s
+ *@needle: string to be found
+ *Return: s + count if found NULL if not
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int count = 0;
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+			if (haystack[i] != needle[j])
+			{
+				count++;
+				break;
+			}
+			else
+				i++;
+		if (needle[j] == '\0')
+			return (haystack + count);
+	}
+	return (0);
+}
