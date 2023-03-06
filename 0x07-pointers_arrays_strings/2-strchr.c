c90fa1e442da28118ebd0c1f54619e389bb5a602
Mbokola <tonypeaks@gmail.com>
2023-03-06 14:34:43 +0000

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  *_strchr - Locate char c in @s
  *@s: pointer to pointer f
@@ -13,5 +12,5 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 		if (s[i] == c)
 			return (s + i);
-	return (NULL);
+	return ('\0');
 }
