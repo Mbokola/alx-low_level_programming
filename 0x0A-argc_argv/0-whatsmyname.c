159c5038d4587de8000f57d3723b48853beb518b
Mbokola <tonypeaks@gmail.com>
2023-03-10 21:30:47 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - program entry
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: o if succesful
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
