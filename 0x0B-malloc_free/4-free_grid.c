206cc537a30747aad75780e5fa06ba5954d3876b
Mbokola <tonypeaks@gmail.com>
2023-03-14 22:17:05 +0000

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ *free_grid - frees allocated memeory
+ *@grid: to be freed
+ *@height: Number of rows
+ *Return: 2 dimensional array
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < h; i++)
+		free(grid[i]);
+	free(grid);
+}
