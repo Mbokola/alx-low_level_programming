f6db93636faa5912217d405a84b2cceaa22ac654
Mbokola <tonypeaks@gmail.com>
2023-03-21 17:14:25 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _struct
+#define _struct
+/**
+ *struct dog - dog structure
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
