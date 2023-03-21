c7caaaf3aada2e67f58c3755664b46de35c68e88
Mbokola <tonypeaks@gmail.com>
2023-03-21 18:34:28 +0000

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,15 +10,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	/* allocate memory for struct dog */
-
-	d = malloc(sizeof(struct dog));
-
-	/* if malloc fails */
-
-	if(!d)
-		exit(0);
-
 	/* initialize dog name, age and owner */
 	d->name = name;
 	d->age = age;
