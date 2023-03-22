0fc4fd14ceb52e3fbfa34e1339322d755f6fa488
Mbokola <tonypeaks@gmail.com>
2023-03-22 21:31:44 +0000

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointer.h"
+/**
+ *int_index - checks index of first value @cmp does not return 0
+ *@array: array values to be checked
+ *@size: size of array
+ *@cmp: function pointer
+ *
+ *Return: index if true, -1 if no match or size <= 0
+ */
+int int_index(int *array, int size, int(*cmp)(int))
+{
+	int i; /* iteration variable */
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp) /* check for null input */
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
