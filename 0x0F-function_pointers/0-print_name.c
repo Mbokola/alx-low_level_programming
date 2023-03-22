c93b712a65752e1c7665c68a468431572da9c04c
Mbokola <tonypeaks@gmail.com>
2023-03-22 18:36:24 +0000

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - print name of person
+ *@name: name of person
+ *@f: function pointer
+ *
+ *Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
