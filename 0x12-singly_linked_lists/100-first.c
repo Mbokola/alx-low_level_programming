b7e7a40e68b5b240166c070af4a9c74730d6aaef
Mbokola <tonypeaks@gmail.com>
2023-03-30 20:08:44 +0000

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print - prints a sentence
+ *
+ *Return: Nothing
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+               I bore my house upon my back!\n);");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
+void print(void);
 int _putchar(char c);
 int _strlen(const char *s);
 size_t print_list(const list_t *h);
