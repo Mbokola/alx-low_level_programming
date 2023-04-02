7dc93ffb6616bf0e352f3e7409154a8d86e07102
Mbokola <tonypeaks@gmail.com>
2023-04-02 10:35:50 +0000

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -22,7 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 		(*head)->str = strdup(str);
 		(*head)->len = strlen(str);
 		(*head)->next = current;
-
-		}
+		return (current);
+	}
 	return (0);
 }
