c5f2d92797b5d09d08b8658b5ae3d5054d6a64d4
Mbokola <tonypeaks@gmail.com>
2023-04-03 08:30:27 +0000

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *print_listint - prints elemenits inside a node
+ *@h: pointer to struct
+ *
+ *Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count;
+	const listint_t *current;
+
+	if(!h)
+		return (0);
+	current = h;
+	for (count = 0; current; count++)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+
+	return (count);
+}
