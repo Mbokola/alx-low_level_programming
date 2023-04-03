7b04cca3ec56a1324b3fbbda06170ed6be4b9ff1
Mbokola <tonypeaks@gmail.com>
2023-04-03 13:40:48 +0000

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - frees a list
+ *@head: pointer to a list
+ *
+ *Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *set_free;
+
+	if (*head)
+	{
+		for (; *head; free(set_free))
+		{
+			set_free = *head;
+			*head = (*head)->next;
+		}
+	}
+	*head = NULL;
+}
