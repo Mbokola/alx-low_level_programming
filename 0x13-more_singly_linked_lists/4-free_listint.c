28666c23e4efe4dbcdcd6c008f7f852c70a5884c
Mbokola <tonypeaks@gmail.com>
2023-04-03 13:46:23 +0000

    Task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,12 +10,9 @@ void free_listint(listint_t *head)
 {
 	listint_t *set_free;
 
-	if (head)
+	for (; head; free(set_free))
 	{
-		for (; head; free(set_free))
-		{
-			set_free = head;
-			head = head->next;
-		}
+		set_free = head;
+		head = head->next;
 	}
 }
