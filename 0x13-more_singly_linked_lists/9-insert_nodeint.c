c0272684a894527a3fa2ec450eb95575a5755332
Mbokola <tonypeaks@gmail.com>
2023-04-03 21:20:07 +0000

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	tmp = *head;
-	for (counter = 0; counter < idx; counter++)
+	for (counter = 0; counter < (idx - 1); counter++)
 	{
 		if (tmp)
 			tmp = tmp->next;
