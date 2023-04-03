5915171130a508688e2ef08f917b6471769e987d
Mbokola <tonypeaks@gmail.com>
2023-04-03 18:31:52 +0000

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,10 @@ void free_listint2(listint_t **head)
 	listint_t *set_free;
 
 	if (head == NULL)
-		return (NULL);
+		return;
 	for (; *head; free(set_free))
 	{
 		set_free = *head;
 		*head = (*head)->next;
 	}
-	*head = NULL;
 }
