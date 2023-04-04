9aa9cc5abafdf16d6d67ee19f17361422bc0256f
Mbokola <tonypeaks@gmail.com>
2023-04-04 09:58:22 +0000

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,6 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new_node->next = tmp->next;
 			tmp->next = new_node;
+			return (new_node);
 		}
 		tmp = tmp->next;
 	}
