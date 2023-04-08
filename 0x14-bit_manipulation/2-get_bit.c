65cec5abea02ac9fbba84c3bd3d1db6e742f930a
Mbokola <tonypeaks@gmail.com>
2023-04-08 09:12:25 +0000

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,14 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int j, count;
 
-	for (count = 0; n >> count >= 1; count++)
-		;
+	if (index <= count)
+	{
 		j = n >> index;
 
 		if (j & 1)
 			return (1);
 		else
 			return (0);
+	}
 	return (-1);
 }
