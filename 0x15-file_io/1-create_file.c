97bcb11b02517cf7c8f3fd746b6dabf2486eb9dd
Mbokola <tonypeaks@gmail.com>
2023-04-10 21:22:35 +0000

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,18 @@
 #include "main.h"
+/**
+ *create_file - creates a file
+ *@filename: file of interest
+ *@text_content: contents to write into @filename
+ *
+ *Return: -1 if fail 1 if success
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i = 0;
 	char c;
 
 	if (!filename)
-		return (1);
+		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
