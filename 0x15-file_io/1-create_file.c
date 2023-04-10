81040d38872d40897e99367236e77713885c9c04
Mbokola <tonypeaks@gmail.com>
2023-04-10 21:26:01 +0000

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
+	if (!text_content)
+		return (1);
 	while (text_content[i])
 	{
 		c = text_content[i];
