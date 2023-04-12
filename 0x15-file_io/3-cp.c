b54190ae638e191a12af34efcae4aa51806a0492
Mbokola <tonypeaks@gmail.com>
2023-04-12 18:36:31 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,6 +31,8 @@ int main(int ac, char **av)
 	}
 	while ((read_in = read(fd, buffer, 1024)) > 0)
 		write(fd1, buffer, read_in);
+	if (read_in < 0)
+		exit(98);
 	if (close(fd) < 0 || close(fd1) < 0)
 		return (100);
 	free(buffer);
