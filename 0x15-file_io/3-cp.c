7947619fec8efb1b1563c82108b7b1b1d5707586
Mbokola <tonypeaks@gmail.com>
2023-04-12 19:18:58 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,6 +37,11 @@ int main(int ac, char **av)
 			exit(99);
 		}
 	}
+	if (read_in < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	if (close(fd) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
