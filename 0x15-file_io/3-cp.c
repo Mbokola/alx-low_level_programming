d6123a5ead0a43b7d464e57fa800b08ad1fa459a
Mbokola <tonypeaks@gmail.com>
2023-04-13 09:21:43 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,21 +17,15 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
+	if ((fd = open(av[1], O_RDONLY)) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	fd1 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	while ((read_in = read(fd, buffer, 1024)) > 0)
 	{
-		if (write(fd1, buffer, read_in) < 0)
+		if (write(fd1, buffer, read_in) < 0 || fd1 < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
@@ -52,5 +46,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
 		exit(100);
 	}
+	free(buffer);
 	return (0);
 }
