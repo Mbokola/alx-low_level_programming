913a40f929622efc83cafd9619a3e899044baf67
Mbokola <tonypeaks@gmail.com>
2023-04-13 09:24:32 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,8 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if ((fd = open(av[1], O_RDONLY)) == -1)
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
