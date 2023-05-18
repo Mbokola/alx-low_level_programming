a0048f634472276e408eedbb19eb10603e18854a
Mbokola <tonypeaks@gmail.com>
2023-05-18 12:32:05 +0000

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *current, *new_node = malloc(sizeof(dlistint_t));
 	/* Error check malloc */
 	if (!new_node)
-		exit(EXIT_FAILURE);
+		return (NULL);
 	new_node->n = n;
 	/* set new_node, prev node to null */
 	new_node->prev = NULL;
