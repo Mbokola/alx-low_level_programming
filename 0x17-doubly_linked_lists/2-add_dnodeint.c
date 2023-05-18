9bd2af5489196c3d027cc033e4a0378b47707332
Mbokola <tonypeaks@gmail.com>
2023-05-18 11:55:08 +0000

    Task 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ *add_dnodeint - adds node at beginning of list
+ *@head: pointer to list
+ *@n: data value of node
+ *
+ *Return: New list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	/* create new_node */
+	dlistint_t *current, *new_node = malloc(sizeof(dlistint_t));
+
+	new_node->n = n;
+	/* set new_node, prev node to null */
+	new_node->prev = NULL;
+	/* check if list is empty */
+	if (!*head)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	/* Check we are at beginning of list */
+	current = *head;
+	while (current->prev)
+		current = current->prev;
+	/* set new_node, next node to head */
+	new_node->next = current;
+	/* set head node, prev node to new_node */
+	current->prev = new_node;
+	/* Make new_node the head node */
+	current = new_node;
+	*head = current;
+	/* Return list */
+	return (*head);
+}
--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -1,4 +1,31 @@
 #include "lists.h"
+/**
+ *print_dlistint - prints elements in a list
+ *@h: pointer to list
+ *
+ *Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+
+        size_t count = 0; /* Keeps count of visited nodes */
+        const dlistint_t *current; /* Pointer to traverse with */
+
+        current = h;
+        if (!current) /* Check if list is empty */
+                return (0);
+        while (current->prev) /* Check we are at beginning of list */
+                current = current->prev;
+        while (current)
+        {
+                count += 1;
+                printf("%d\n", current->n);
+                if (!current->next)
+                        break;
+                current = current->next;
+        }
+        return (count);
+}
 
 /**
  * main - check the code
@@ -8,24 +35,16 @@
 int main(void)
 {
     dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
 
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = dlistint_len(head);
-    printf("-> %lu elements\n", n);
-    free(new);
+    head = NULL;
+    add_dnodeint(&head, 0);
+    add_dnodeint(&head, 1);
+    add_dnodeint(&head, 2);
+    add_dnodeint(&head, 3);
+    add_dnodeint(&head, 4);
+    add_dnodeint(&head, 98);
+    add_dnodeint(&head, 402);
+    add_dnodeint(&head, 1024);
+    print_dlistint(head);
     return (EXIT_SUCCESS);
 }
