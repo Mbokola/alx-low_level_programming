616495677b66b7d9a3f2964a9cd7b11d2531dc82
Mbokola <tonypeaks@gmail.com>
2023-05-18 13:30:46 +0000

    Task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,20 +8,11 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *save_link;
-	/* Check if list is empty */
-	if (head)
+
+	while (head)
 	{
-	/* Save_link to next node */
 		save_link = head->next;
-	/* Free head node */
 		free(head);
-	/* Set head node to save_link, this marks begginging of loop*/
-		while (save_link->next)
-		{
-			head = save_link;
-			save_link = save_link->next;
-			free(head);
-		}
-		free(save_link);
+		head = save_link;
 	}
 }
