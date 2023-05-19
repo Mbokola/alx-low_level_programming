3448e6de30efff37ba58693168594409a294e0b7
Mbokola <tonypeaks@gmail.com>
2023-05-19 19:07:04 +0000

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- *delete_dnodeint_at_index - function that deletes the node at index @index of a dlistint_t linked list.
+ *delete_dnodeint_at_index - function that deletes the node at index @index of
+ *a dlistint_t linked list.
  *@head: doubly linked list
  *@index: index of the node that should be deleted. Index starts at 0.
  *
@@ -26,13 +27,22 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	for (; index; index--)
 	{
 		current = current->next;
-		/* Return -1 if index is out of range i.e when current is set to NULL after reaching end of the list */
+		/*
+		 * Return -1 if index is out of range i.e when current is set
+		 * to NULL after reaching end of the list
+		 */
 		if (!current)
 			return (-1);
 	}
-	/* Set next pointer of node before/next current node to node after/prev current node */
+	/*
+	 * Set next pointer of node before/next current node to node after/prev
+	 * current node
+	 */
 	current->prev->next = current->next;
-	/* Set prev node of node after/next current node to node before/prev current node */
+	/*
+	 * Set prev node of node after/next current node to
+	 * node before/prev current node
+	 */
 	current->next->prev = current->prev;
 	/* Free current node */
 	free(current);
