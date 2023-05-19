efcdd5f45dda8118580d1a8bfb226113c7cce78b
Mbokola <tonypeaks@gmail.com>
2023-05-19 11:26:18 +0000

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,7 +39,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = current;
 	new_node->next = current->next;
 	current->next = new_node;
-	new_node->next->prev = new_node;
+	if (!current->next)
+		new_node->next->prev = new_node;
 	/* Return List */
 	return (new_node);
 }
