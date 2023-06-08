780c827c53e9eef4e9796107e6810a153e9e3bc6
Mbokola <tonypeaks@gmail.com>
2023-06-08 19:15:45 +0000

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	unsigned long int i;
 
-	if (!table)
+	if (!table || !size)
 		return (NULL);
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t));
 	if (!table->array)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
 	return (table);
 }
