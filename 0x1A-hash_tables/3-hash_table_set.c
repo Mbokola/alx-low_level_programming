8de0ec670c9ac38afaaf2251fd16f9cb68edfb52
Mbokola <tonypeaks@gmail.com>
2023-06-09 12:58:20 +0000

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_set - function that adds an element to the hash table.
+ *@ht: the hash table
+ *@key: key to insert
+ *@value:value to inset
+ *
+ *Return: 1 if succesful 0 if otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node = NULL, *ptr = NULL;
+	char *k = (char *)key;
+	const unsigned char *k2 = (const unsigned char *)key;
+	char *val = (char *)value;
+	unsigned  long int index;
+
+	index = hash_djb2(k2) % ht->size;
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	node->key = k;
+	node->value = val;
+	ptr = ht->array[index];
+	if (!ptr)
+	{
+		node->next = ptr;
+		ht->array[index] = node;
+	}
+	else
+	{
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = node;
+		node->next = NULL;
+	}
+	return (1);
+}
