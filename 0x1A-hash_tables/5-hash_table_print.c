74bd03e1c0ee8d36f314afefc36b767a3034a771
Mbokola <tonypeaks@gmail.com>
2023-06-09 19:12:05 +0000

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (print)
 					printf(", ");
-				printf("'%s' : '%s'", ptr->key, ptr->value);
+				printf("'%s': '%s'", ptr->key, ptr->value);
 				ptr = ptr->next;
 				print++;
 			}
