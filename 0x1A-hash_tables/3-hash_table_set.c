e528fe3fe2128306f16ba938eeb305c673e5446a
Mbokola <tonypeaks@gmail.com>
2023-06-09 18:18:20 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,19 +10,29 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = NULL, *ptr = NULL;
+	hash_node_t *node = NULL;
 	unsigned  long int index;
 
-	if (!ht)
+	if (!ht || !key || !strlen(key))
 		return (0);
 	index = hash_djb2((const unsigned char *)key) % ht->size;
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
 	node->key = strdup((char *)key);
+	if (!node->key)
+	{
+		free(node);
+		return (0);
+	}
 	node->value = strdup((char *)value);
-	ptr = ht->array[index];
-	node->next = ptr;
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
 }
