54bb09e4282fb713fc9f85caa17a26489621b781
Mbokola <tonypeaks@gmail.com>
2023-06-09 18:19:32 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,38 +1,44 @@
 #include "hash_tables.h"
 
 /**
- *hash_table_set - function that adds an element to the hash table.
- *@ht: the hash table
- *@key: key to insert
- *@value:value to inset
+ * hash_table_set - adds and element to the hash table
+ * @ht: the new hash table to be added
+ * @key: is the key
+ * @value: that will be store in the table
  *
- *Return: 1 if succesful 0 if otherwise
+ * Return: 1 if successful else 0
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = NULL;
-	unsigned  long int index;
+	hash_node_t *node_to_add;
+	char *new_value;
+	unsigned long int index;
 
-	if (!ht || !key || !strlen(key))
-		return (0);
-	index = hash_djb2((const unsigned char *)key) % ht->size;
-	node = malloc(sizeof(hash_node_t));
-	if (!node)
-		return (0);
-	node->key = strdup((char *)key);
-	if (!node->key)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 	{
-		free(node);
 		return (0);
 	}
-	node->value = strdup((char *)value);
-	if (!node->value)
+	index = key_index((const unsigned char *)key, ht->size);
+	node_to_add = malloc(sizeof(hash_node_t));
+	if (node_to_add == NULL)
+		return (0);
+	new_value = strdup(value);
+	if (new_value == NULL)
 	{
-		free(node->key);
-		free(node);
+		free(node_to_add);
 		return (0);
 	}
-	node->next = ht->array[index];
-	ht->array[index] = node;
+	node_to_add->key = strdup(key);
+	node_to_add->value = new_value;
+	node_to_add->next = NULL;
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = node_to_add;
+	}
+	else
+	{
+		node_to_add->next = ht->array[index];
+		ht->array[index] = node_to_add;
+	}
 	return (1);
 }
