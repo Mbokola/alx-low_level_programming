20ddbcd31b49f84d7401dc58ab5c3c20c074928a
Mbokola <tonypeaks@gmail.com>
2023-07-26 22:36:39 +0000

    Fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,9 +19,6 @@ def island_perimeter(grid):
         for index, value in enumerate(row):
             if value == 1:
                 if index > longest_row:
-                    longest_row = index
-                if index < shortest_row or shortest_row < 0:
-                    shortest_row = index
-    if not shortest_row:
-        longest_row += 1
+                    shortest_row = row.index(1)
+                    longest_row = (index + 1) - shortest_row
     return 2 * (longest_row + count)
